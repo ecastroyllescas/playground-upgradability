b40b8fcaddb29a7c410fc0ceaee060aa251f51d9
invocamanman <jesus.ligero@yahoo.es>
2023-09-22 11:19:36 +0200

    add comments

--- contracts/TransparentPattern/StorageContract.sol
+++ contracts/TransparentPattern/StorageContract.sol
@@ -5,6 +5,10 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 contract StorageContract is OwnableUpgradeable {
     uint256 value;
 
+    // Be aware when writing upgradable smart contract to not user a constructor ( if used, only for immutables!) and use an initializer instead!
+    // The initialize whould be all the "constructor" setup variables, and be aware that, similar to a constructor
+    // you must initialize all the libs that required to!
+    // Since the initialize function should be only called once, OZ has this modifier `initializer` just for this, remember to add it!
     function initialize(uint256 _value) public initializer {
         value = value;
 
--- contracts/UUPS/StorageContract.sol
+++ contracts/UUPS/StorageContract.sol
@@ -6,6 +6,10 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 contract StorageContractUUPS is UUPSUpgradeable, OwnableUpgradeable {
     uint256 value;
 
+    // Be aware when writing upgradable smart contract to not user a constructor ( if used, only for immutables!) and use an initializer instead!
+    // The initialize whould be all the "constructor" setup variables, and be aware that, similar to a constructor, you
+    // must initialize all the libs that required to!
+    // Since the initialize function should be only called once, OZ has this modifier `initializer` just for this, remember to add it!
     function initialize(uint256 _value) public initializer {
         value = value;
 
@@ -13,6 +17,8 @@ contract StorageContractUUPS is UUPSUpgradeable, OwnableUpgradeable {
         __Ownable_init();
     }
 
-    // Mandatory override for UUPS proxies
+    // Mandatory override for UUPS proxies, here should be the authentication logic to upgrade a contract
+    // In this case, the function is empty, but is has the `onlyOwner` modifier, so basically only the owner would be able to upgrade
+    // this contract
     function _authorizeUpgrade(address) internal override onlyOwner {}
 }
